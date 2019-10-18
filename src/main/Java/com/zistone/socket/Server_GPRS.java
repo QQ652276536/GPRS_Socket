51686edd3ddd950d08c0798b2dc70f187ac41d15
QQ652276536 <652276536@qq.com>
2019-10-18 13:38:41 +0800

    no message

--- src/main/Java/com/zistone/socket/Server_GPRS.java
+++ src/main/Java/com/zistone/socket/Server_GPRS.java
@@ -41,11 +41,12 @@ public class Server_GPRS
                 socket.setSoTimeout(HEARTTIMEOUT_SOCKET);
                 //在下发参数设置前有一次设备鉴权后的通用应答
                 m_detail += 2;
-                m_logger.debug(">>>----------创建一个Server_GPRS_Worker线程----------");
                 Server_GPRS_Worker server_gprs_woker = new Server_GPRS_Worker(socket, m_detail, m_setData);
-                Thread thread = new Thread(server_gprs_woker);
-                thread.setDaemon(true);
-                thread.start();
+                server_gprs_woker.Worker();
+                //                Thread thread = new Thread(server_gprs_woker);
+                //                thread.setDaemon(true);
+                //                thread.start();
+                //                m_logger.debug(">>>----------Server_GPRS_Worker线程启动----------");
             }
             catch (Exception e)
             {
